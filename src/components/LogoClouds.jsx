8136d662acf35cca6f68e0e3885b4a5057bd2225
varia <ltnhan2610@gmail.com>
2024-01-03 15:06:43 +0700

    style LogoClouds container

--- src/components/LogoClouds.jsx
+++ src/components/LogoClouds.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 
 function LogoClouds() {
-	return <div>
-        <h3>Trusted by brand all over the world</h3>
-        <div>
-            <ul>
-                <li>slack</li>
-                <li>dropbox</li>
-                <li>spotify</li>
-                <li>amazon</li>
-                <li>netflix</li>
-                <li>stripe</li>
-            </ul>
-        </div>
-    </div>;
+	return (
+		<div className="py-20">
+			<h3 className="text-3xl text-slate-400 my-7 text-center">
+				Trusted by brand all over the world
+			</h3>
+			<div>
+				<ul className="flex justify-center gap-10">
+					<li>
+						<div>
+							<img src="/logos/slack-gray.svg" alt="slack" />
+						</div>
+					</li>
+					<li>
+						<div>
+							<img src="/logos/dropbox-gray.svg" alt="dropbox" />
+						</div>
+					</li>
+					<li>
+						<div>
+							<img src="/logos/spotify-gray.svg" alt="spotify" />
+						</div>
+					</li>
+					<li>
+						<div>
+							<img src="/logos/amazon-gray.svg" alt="amazon" />
+						</div>
+					</li>
+					<li>
+						<div>
+							<img src="/logos/netflix-gray.svg" alt="netflix" />
+						</div>
+					</li>
+					<li>
+						<div>
+							<img src="/logos/stripe.svg" alt="stripe" />
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default LogoClouds;
