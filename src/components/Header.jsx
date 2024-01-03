dcf4f4cd125d84be793a2263668bb994cb7d7604
varia <ltnhan2610@gmail.com>
2024-01-03 13:14:11 +0700

    add Header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+	return (
+		<div className="max-w-screen-lg mx-auto text-center">
+			<p className="text-lg text-sky-400 font-semibold my-7">
+				What&apos;s new at Shuffle
+			</p>
+			<h1 className="text-7xl font-bold text-sky-900 my-10">
+				Take care of your performance everyday.
+			</h1>
+			<p className="text-2xl text-slate-400 my-7">
+				Build a well-presented brand that everyone will love. Take care
+				to develop resources continually and integrity them with
+				previous projects.
+			</p>
+			<button className="bg-red-500 rounded p-4 text-white hover:bg-red-400 my-7">
+				Track your performance
+			</button>
+		</div>
+	);
 }
 
-export default Header
+export default Header;
