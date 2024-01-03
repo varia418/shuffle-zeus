41b62f66ce8c5818b3703c399f027794f3336b55
varia <ltnhan2610@gmail.com>
2024-01-03 13:17:35 +0700

    add LogoClouds content

--- src/components/LogoClouds.jsx
+++ src/components/LogoClouds.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function LogoClouds() {
-	return <div>LogoClouds</div>;
+	return <div>
+        <h3>Trusted by brand all over the world</h3>
+        <div>
+            <ul>
+                <li>slack</li>
+                <li>dropbox</li>
+                <li>spotify</li>
+                <li>amazon</li>
+                <li>netflix</li>
+                <li>stripe</li>
+            </ul>
+        </div>
+    </div>;
 }
 
 export default LogoClouds;
