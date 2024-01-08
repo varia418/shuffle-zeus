b6973aad99cf0934d4861a75a04b6a57f1bae9eb
varia <ltnhan2610@gmail.com>
2024-01-08 13:28:24 +0700

    fix attribute names

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ function NavBar() {
 
 				<button className="md:hidden" onClick={toggleMobileNav}>
 					<svg
-						class="block h-4 w-4"
+						className="block h-4 w-4"
 						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg"
 						data-config-id="auto-svg-1-2"
@@ -56,7 +56,7 @@ function NavBar() {
 						/>
 						<button className="close-btn" onClick={toggleMobileNav}>
 							<svg
-								class="h-6 w-6 text-gray-500 cursor-pointer hover:text-gray-500"
+								className="h-6 w-6 text-gray-500 cursor-pointer hover:text-gray-500"
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
@@ -64,9 +64,9 @@ function NavBar() {
 								data-config-id="auto-svg-2-2"
 							>
 								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
 									d="M6 18L18 6M6 6l12 12"
 								></path>
 							</svg>
